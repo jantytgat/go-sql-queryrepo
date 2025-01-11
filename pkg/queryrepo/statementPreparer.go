0d49666e6f5bce118e5b4ecaa6b8b38fb6727dfc
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-01-11 15:44:31 +0100

    Update Prepare to accept pointer to repository
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/queryrepo/statementPreparer.go
+++ pkg/queryrepo/statementPreparer.go
@@ -6,7 +6,7 @@ type StatementPreparer interface {
 	Prepare(query string) (*sql.Stmt, error)
 }
 
-func Prepare[T StatementPreparer](t T, r Repository, collectionName, statementName string) (*sql.Stmt, error) {
+func Prepare[T StatementPreparer](t T, r *Repository, collectionName, statementName string) (*sql.Stmt, error) {
 	var err error
 	var statement string
 
