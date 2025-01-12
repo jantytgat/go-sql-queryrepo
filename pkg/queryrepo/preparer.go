b239ee0b34f864a1f431ff015a70535a22fb6355
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-01-12 21:07:48 +0100

    Update godoc for Preparer
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/queryrepo/preparer.go
+++ pkg/queryrepo/preparer.go
@@ -5,10 +5,13 @@ import (
 	"io/fs"
 )
 
+// Preparer defines the interface to create a prepared statement
 type Preparer interface {
 	Prepare(query string) (*sql.Stmt, error)
 }
 
+// Prepare creates a prepared statement for the supplied Preparer by looking up a query in the supplied repository.
+// It returns an nil pointer and an error if either the query cannot be found in the supplied repository, or the statement preparation fails.
 func Prepare[T Preparer](t T, r *Repository, collectionName, queryName string) (*sql.Stmt, error) {
 	var err error
 	var query string
@@ -19,6 +22,8 @@ func Prepare[T Preparer](t T, r *Repository, collectionName, queryName string) (
 	return t.Prepare(query)
 }
 
+// PrepareFromFs creates a prepared statement for the supplied Preparer by looking up a query in the supplied filesystem.
+// It returns an nil pointer and an error if either the query cannot be found in the supplied filesystem, or the statement preparation fails.
 func PrepareFromFs[T Preparer](t T, f fs.FS, rootPath, collectionName, queryName string) (*sql.Stmt, error) {
 	var err error
 	var query string
