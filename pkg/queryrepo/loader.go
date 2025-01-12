a49ee4d7d3e3285f6a2ba66d104a44e30f70d934
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-01-12 20:29:03 +0100

    Update godoc for LoadQueryFromFs
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/queryrepo/loader.go
+++ pkg/queryrepo/loader.go
@@ -6,6 +6,11 @@ import (
 	"path/filepath"
 )
 
+// LoadQueryFromFs retrieves a query from a filesystem.
+// It needs the root path to start the search from, as well as a collection name and a query name.
+// The collection name equals to a direct directory name in the root path.
+// The query name is the file name (without extension) to load the contents from.
+// It returns and empty string and an error if the file cannot be found.
 func LoadQueryFromFs(f fs.FS, rootPath, collectionName, queryName string) (string, error) {
 	var err error
 	var contents []byte
